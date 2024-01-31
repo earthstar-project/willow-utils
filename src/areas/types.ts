3716b3914febdcde693f28e5cd3c8f9cfe27a3e6
Sam Gwilym <sam@gwil.garden>
2024-01-31 14:17:11 +0100

    add AreaOfInterest type

--- src/areas/types.ts
+++ src/areas/types.ts
@@ -14,3 +14,13 @@ export type Area<SubspaceType> = {
   /** To be included in this Area, an Entry’s subspace_id must be equal to the included_subspace_id, unless it is any. */
   includedSubspaceId: SubspaceType | typeof ANY_SUBSPACE;
 };
+
+/** A grouping of Entries that are among the newest in some store. */
+export type AreaOfInterest<SubspaceType> = {
+  /** To be included in this AreaOfInterest, an Entry must be included in the area. */
+  area: Area<SubspaceType>;
+  /** To be included in this AreaOfInterest, an Entry’s timestamp must be among the max_count greatest Timestamps, unless max_count is zero. */
+  maxCount: number;
+  /** The total payload_lengths of all included Entries is at most max_size, unless max_size is zero. */
+  maxSize: number;
+};
