35c29bbe7f02e179686a2ec4027c3912d342b4e6
Sam Gwilym <sam@gwil.garden>
2024-03-13 16:58:53 +0100

    Add PrivyEncodingScheme type

--- src/encoding/types.ts
+++ src/encoding/types.ts
@@ -10,3 +10,25 @@ export type EncodingScheme<ValueType> = {
   encodedLength: (value: ValueType) => number;
   decodeStream: StreamDecoder<ValueType>;
 };
+
+export type PrivyEncodingScheme<ValueType, PrivyType> = {
+  // e.g. Value type here is a SetupBindReadCapability
+  // the privy type is what both sides know - in this case,
+  // the outer area and the namespace.
+  encode: (value: ValueType, privy: PrivyType) => Uint8Array;
+  encodedLength: (value: ValueType, privy: PrivyType) => number;
+
+  // Although it would seem natural to put the privy type in the params,
+  // Before calling this function we cannot know what this message is privy to.
+  // e.g. if this is an encoded SetupBindReadCapability
+  // then we can only know what we are privy to once we have the intersection handle
+  // encoded in the message.
+  // we then need to dereference that handle to get the privy info
+  // e.g. the outer area of the intersection handle.
+  // sometimes we have to dereference something in the message,
+  // sometimes we don't (e.g. in reconciliation messages.)
+
+  decode: (encoded: Uint8Array, privy: PrivyType) => ValueType;
+
+  decodeStream: (bytes: GrowingBytes, privy: PrivyType) => Promise<ValueType>;
+};
